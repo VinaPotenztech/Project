f4fd7843b27dccc5cf498b24d0aaadf4ad0f71cf
VinaPotenztech <Vina@Potenztech.com>
2024-08-30 16:00:47 +0530

    change

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,7 +2,6 @@ const User=require('../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt=require('jsonwebtoken')
 const {JWT_SECRET}=require('../utils/jwtUtils');
-const { error } = require('console');
 
 // @desc    Register a new user
 // @route   POST /api/auth/register
