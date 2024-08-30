948667e73ccef750b1006dc2de565670d4a3e731
VinaPotenztech <Vina@Potenztech.com>
2024-08-30 17:09:19 +0530

    edited code

--- Middleware/authMiddlerware.js
+++ Middleware/authMiddlerware.js
@@ -2,7 +2,7 @@ const jwt=require('jsonwebtoken')
 const {JWT_SECRET}=require('../utils/jwtUtils')
 
 const verifyToken=(req,res,next)=>{
-    const token=req.headers['authorization']?.split('')[1];
+    const token=req.headers['authorization']?.split(' ')[1];
     if(!token)return res.status(403).json({error:'Token required'})
 
     jwt.verify(token,JWT_SECRET,(err,decoded)=>{
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,6 @@ const {registerUser,loginUser}=require('../controllers/authControllers');
 const router=express.Router();
 const apiKeyMiddleware = require('../Middleware/apiKeyMiddleware'); // Import the API key middleware
 
-router.use(apiKeyMiddleware)
 router.post('/register',registerUser);
 router.post('/login',loginUser);
 
